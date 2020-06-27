4a5ce8dd65b190874a22dde9cef117cdf8f8c52a
tieonlinux <xtieonlinux@gmail.com>
2020-06-27 11:39:49 +0200

    Splited container setup into 2 script to use the docker cache in a better way

--- fs/0.setup_system.sh
+++ fs/0.setup_system.sh
@@ -13,20 +13,6 @@ apt-get install -y --no-install-recommends wget unzip rsync dumb-init sudo bash 
 # update mono cert store
 timeout 120 cert-sync /etc/ssl/certs/ca-certificates.crt || :
 
-# setup tshock
-if [ -z "${TSHOCK_URL}" ]; then
-    echo "Missing TSHOCK_URL env variable !"
-    exit 1
-fi
-mkdir -p "/config" "/world" "/logs" "/plugins" "/tshock" &&
-cd /tshock &&
-echo "downloading tshock" &&
-wget "$TSHOCK_URL" -O tshock.zip --tries=5 &&
-unzip tshock.zip &&
-rm tshock.zip &&
-chmod +x /tshock/TerrariaServer.exe &&
-chmod +x /start.py || exit $?
-
 # install gosu
 dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
 wget --tries=5 -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/1.12/gosu-$dpkgArch" &&
@@ -49,7 +35,6 @@ echo '' > /var/log/alternatives.log
 
 # terraria user & group
 addgroup --system --gid 1001 terraria &&
-adduser --system --uid 1001 --ingroup terraria --shell /bin/bash terraria &&
-chown -R terraria:terraria "/config" "/world" "/logs" "/plugins" "/tshock"
+adduser --system --uid 1001 --ingroup terraria --shell /bin/bash terraria ||
 
 exit $?
