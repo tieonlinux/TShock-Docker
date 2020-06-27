1726b8cec89cfd3173675f1cc55bca8996033ba0
tieonlinux <xtieonlinux@gmail.com>
2020-06-27 11:40:50 +0200

    Changed adduser to useradd command and use --no-log-init

--- fs/0.setup_system.sh
+++ fs/0.setup_system.sh
@@ -35,6 +35,6 @@ echo '' > /var/log/alternatives.log
 
 # terraria user & group
 addgroup --system --gid 1001 terraria &&
-adduser --system --uid 1001 --ingroup terraria --shell /bin/bash terraria ||
+useradd --no-log-init --system --uid 1001 -g terraria --shell /bin/bash terraria ||
 
 exit $?
