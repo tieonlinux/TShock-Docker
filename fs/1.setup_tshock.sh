1d46f80d6e866a4201c3ccfa58c7f6742cda2fae
tieonlinux <xtieonlinux@gmail.com>
2020-06-27 11:39:49 +0200

    Splited container setup into 2 script to use the docker cache in a better way

---
+++ fs/1.setup_tshock.sh
@@ -0,0 +1,17 @@
+# create tshock realted directories
+mkdir -p "/config" "/world" "/logs" "/plugins" "/tshock" &&
+chown -R terraria:terraria "/config" "/world" "/logs" "/plugins" "/tshock" &&
+chmod +x /start.py
+
+if [ -z "${TSHOCK_URL}" ]; then
+    echo "Missing TSHOCK_URL env variable !"
+    exit 1
+fi
+cd /tshock &&
+echo "downloading tshock" &&
+wget "$TSHOCK_URL" -O tshock.zip --tries=5 &&
+unzip tshock.zip &&
+rm tshock.zip &&
+chown -R terraria:terraria "/tshock" &&
+chmod +x /tshock/TerrariaServer.exe ||
+exit $?
